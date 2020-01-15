1153a38ff0c81dd23d73dbf9f19bfa966800f021
et <eric.thomson13@gmail.com>
2020-01-15 10:52:41 -0700

    Filtering Based on ThreatLevel Selection

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,32 +10,58 @@ class App extends React.Component {
     super();
     this.state = {
       filter: 'None',
-      bugs: exampleData,
+			bugs: exampleData,
     };
-    this.filterHandler = this.filterHandler.bind(this);
+		this.filterHandler = this.filterHandler.bind(this);
+		this.bugFilter = this.bugFilter.bind(this);
   }
 
   filterHandler(filter) {
     this.setState({ filter });
   }
 
+	bugFilter() {
+		console.log('filter: ' + this.state.filter)
+		if (this.state.filter === 'None') {
+			return this.state.bugs.map((bug) => (
+				<BugTile
+					bugName={bug.bugName}
+					bugDescription={bug.bugDescription}
+					reportedBy={bug.reportedBy}
+					createdDate={bug.createdDate}
+					assignedTo={bug.assignedTo}
+					threatLevel={bug.threatLevel}
+					key={bug.bugName}
+				/>
+			))
+		} else {
+			
+			const filtered = this.state.bugs.filter(bug => {
+				return bug.threatLevel === this.state.filter
+			})
+			console.log('filtered bugs: ' + filtered[0].threatLevel)
+			return filtered.map(bug => {
+				return (
+					<BugTile
+						bugName={bug.bugName}
+						bugDescription={bug.bugDescription}
+						reportedBy={bug.reportedBy}
+						createdDate={bug.createdDate}
+						assignedTo={bug.assignedTo}
+						threatLevel={bug.threatLevel}
+						key={bug.bugName}
+					/>
+				)
+			})
+		}
+	}
   render() {
     return (
       <table>
         <Nav
           filterHandler={this.filterHandler}
         />
-        {this.state.bugs.map((bug) => (
-          <BugTile
-            bugName={bug.bugName}
-            bugDescription={bug.bugDescription}
-            reportedBy={bug.reportedBy}
-            createdDate={bug.createdDate}
-            assignedTo={bug.assignedTo}
-            threatLevel={bug.threatLevel}
-            key={bug.bugName}
-          />
-        ))}
+        {this.bugFilter()}
       </table>
     );
   }
